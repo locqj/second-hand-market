36b153337e69499c997c80447e0019c0e217bd87
locqj <locqj@163.com>
2017-08-30 23:35:42 +0800

    design index

--- code/m-view/second-head-shop/src/components/App.vue
+++ code/m-view/second-head-shop/src/components/App.vue
@@ -1,9 +1,32 @@
-<template lang="html">
-  <div id="app">
-    <!--你也可以在其他地方使用<router-view></router-view>来创建嵌套路由，详见vue-router文档-->
-    	<router-link to="/foo">Go to Foo</router-link>
-      <router-link to="/bar">Go to Bar</router-link>
-      <div>User {{ $route.params.id }}</div>
-    <router-view></router-view>
-  </div>
+<template>
+  <h1>mint-ui-example</h1>
 </template>
+<script>
+  import { Toast, MessageBox } from 'mint-ui';
+  export default {
+    name: 'app',
+
+    data() {
+      return {
+        sheetVisible: false,
+        actions: [{
+          name: '展示 Toast',
+          method: this.showToast
+        }, {
+          name: '展示 Message Box',
+          method: this.showMsgbox
+        }]
+      };
+    },
+
+    methods: {
+      showToast() {
+        Toast('这是一个 Toast');
+      },
+
+      showMsgbox() {
+        MessageBox('提示', '这是一个 Message Box');
+      }
+    }
+  };
+</script>
--- code/m-view/second-head-shop/src/main.js
+++ code/m-view/second-head-shop/src/main.js
@@ -1,7 +1,9 @@
 import Vue from 'vue';
 import App from './components/App.vue';
 import router from './router';
-
+import MintUI from 'mint-ui';
+import 'mint-ui/lib/style.css';
+Vue.use(MintUI);
 new Vue({
   el: '#app',
   router,
